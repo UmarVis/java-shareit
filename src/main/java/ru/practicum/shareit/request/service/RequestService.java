ed864630890bd36fabcf4ff419a6cb49d864b859
umarvis <umar_9292@mail.ru>
2023-04-02 21:21:01 +0300

    feat: добавить запрос вещи, добавить тесты

--- src/main/java/ru/practicum/shareit/request/service/RequestService.java
+++ src/main/java/ru/practicum/shareit/request/service/RequestService.java
@@ -7,7 +7,10 @@ import java.util.List;
 
 public interface RequestService {
     ItemRequestDtoOut create(Integer userId, ItemRequestDtoIn itemRequestDtoIn);
+
     List<ItemRequestDtoOut> getRequestByUser(Integer userId);
+
     ItemRequestDtoOut getById(Integer userId, Integer id);
+
     List<ItemRequestDtoOut> getAll(Integer userId, int from, int size);
 }
